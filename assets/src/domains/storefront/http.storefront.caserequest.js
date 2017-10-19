b409f288486265bb795eba7c7f5b2e493dcfc2d8
Nick Qi <nick@MacBook-Pro-2.local>
2017-10-19 15:18:05 +1100

    add http request

--- assets/src/domains/storefront/http.storefront.caserequest.js
+++ assets/src/domains/storefront/http.storefront.caserequest.js
@@ -1,5 +1,5 @@
 // var rp = require('request-promise');
-var rp = require('./../../../lib/request-promise');
+// var rp = require('./../../../lib/request-promise');
 
 module.exports = function (context, callback) {
 
@@ -24,7 +24,7 @@ module.exports = function (context, callback) {
     method: 'POST',
     uri: 'https://cs58.salesforce.com/services/apexrest/api/Case',
     auth: {
-        'bearer': "00D0l000000CuYe!ARsAQD0CCs0JAmwuQ86uCH30qCBxeXm8sX55UMQLVJBRNMDUQ0S8r_x3XhQKyPydc6v0ZmH2SchrBKVM7csYumMrVfUpUh86"
+        'bearer': "00D0l000000CuYe!ARsAQNSwDA7Y4l7vIwkzkuW.WHRZoWZDi6tu8ce9pfL6EcYeTtW_LRJmkPEO3KGU0zWQ1RlbXgUU.uuUjE92FVHgM7fmd0h9"
     },
     body : data,
     json: true
@@ -36,14 +36,43 @@ module.exports = function (context, callback) {
     "caseID": "01425186"
   };
 
-  rp(addCase)
-  .then(function (response) {
-    console.info(response);
-  })
-  .catch(function (err) {
-    console.info(err);
-  });
+  // context.response.body = caserequest;
 
-  context.response.body = caserequest;
+  // rp(addCase)
+  // .then(function (response) {
+  //   console.info(response);
+  // })
+  // .catch(function (err) {
+  //   console.info(err);
+  // });
+
+  var http = require('http');
+  var options = {
+    hostname: 'https://cs58.salesforce.com',
+    path: '/services/apexrest/api/Case',
+    port: 80,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': data.length
+    },
+    body: data,
+    auth: {
+      'bearer': "00D0l000000CuYe!ARsAQNSwDA7Y4l7vIwkzkuW.WHRZoWZDi6tu8ce9pfL6EcYeTtW_LRJmkPEO3KGU0zWQ1RlbXgUU.uuUjE92FVHgM7fmd0h9"
+    }
+  };
+  var google = {
+    hostname: 'www.google.com',
+    port: 80,
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  };
+  var req = http.request(options, function(res) {
+    context.response.body = res;
+    // callback();
+  });
+  req.end();
   callback();
 };
